b2824c6047845d0113fd8f4404ecf1f13d5e3e1f
ernstmul <ernst.mulders@gmail.com>
2025-03-19 16:30:31 +0100

    Swap pink v1 for tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href={AppwriteIcon} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:opsz,wght@14..32,100..900&family=Poppins&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <body>{children}</body>
+      <body className={"bg-[#FAFAFB] font-[Inter] text-sm text-[#56565C]"}>
+        {children}
+      </body>
     </html>
   );
 }
